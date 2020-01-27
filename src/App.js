602e127710cc8373f52d090964acbe4173f6491a
Gary Liang <gary.liang@nab.com.au>
2020-01-27 22:48:36 +1100

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function Example() {
+  const testFunc = event => {
+    console.log("=== event fire ===");
+  };
+
+  useEffect(() => {
+    window.addEventListener("TEST_EVENT", event => testFunc(event));
+
+    return () => {
+      window.removeEventListener("TEST_EVENT", event => testFunc(event));
+    };
+  }, []);
+  return <div>example</div>;
+}
 
 function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setShow(!show);
+
+          // QUESTION: is it a way not to use setTimeout
+          setTimeout(function() {
+            window.dispatchEvent(
+              new CustomEvent("TEST_EVENT", {
+                bubbles: true,
+                composed: true,
+                detail: {
+                  text: "data"
+                }
+              })
+            );
+          }, 2000);
+        }}
+      >
+        Show me
+      </button>
+      {show && (
+        <div>
+          <Example />
+        </div>
+      )}
     </div>
   );
 }
